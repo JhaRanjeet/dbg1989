cbdd4c8d628464e670c7fb638f51ca3da85d995d
JhaRanjeet <ranjeetjha189@gmail.com>
2026-08-16 17:49:18 +0530

    Update 05_dict.py
    updated script

--- 05_dict.py
+++ 05_dict.py
@@ -1,16 +1,1 @@
-marks={
-    "Hindi":40,
-    "Math" :78,
-    "science":89
-
-}
-print(marks)
-print(marks.get("Hindi"))
-print(marks.get("Eng"))
-# add imtes in dictonary
-marks["Eng"]=98
-print(marks)
-del marks["Hindi"]
-print(marks)
-marks["Hind="]=86
-print(marks)
+print("Hello buddy version 2")
